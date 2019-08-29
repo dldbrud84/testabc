52f204eb9747ac1e4b424dbb30915dfc1ee2d905
dldbrud84 <52441531+dldbrud84@users.noreply.github.com>
2019-08-29 22:02:14 +0900

    Delete main.tf

--- main.tf
+++
@@ -1,6 +0,0 @@
-provider "azurerm" {
-    subscription_id = "e0fae348-f6c2-45f5-87b7-c41c22782d8f"
-    client_id       = "44c1eb23-cbd7-4cfb-80a6-7138bb0ecec5"
-    client_secret   = "0Ko5Uizgj/OoXezW6B0iCighodrex@@?"
-    tenant_id       = "96e3cac9-1ab3-436b-9f79-2a0a4b687f1b"
-}
