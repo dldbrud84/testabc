7c6c9f5af1c7b0942d842c4d847317ba5ce087bc
dldbrud84 <52441531+dldbrud84@users.noreply.github.com>
2019-08-29 22:04:29 +0900

    Delete subnet2.tf

--- subnet2.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "user19-subnet2" {
-  name = "group03-user19-subnet2"
-  resource_group_name = "${azurerm_resource_group.group03-user19.name}"
-  virtual_network_name = "${azurerm_virtual_network.group03-user19-vnetwork.name}"
-  address_prefix = "3.4.2.0/24"
-}
